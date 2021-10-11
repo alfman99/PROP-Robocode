246353e69869771141ee02d62efe591ef576e5b4
alfman99 <amanresamntz@gmail.com>
2021-10-11 18:23:19 +0200

    arreglado tonterias

--- Robocode/src/PROP/robot/Mrpotato.java
+++ Robocode/src/PROP/robot/Mrpotato.java
@@ -7,30 +7,45 @@ import robocode.*;
  * @author srimp
  */
 public class Mrpotato extends AdvancedRobot {
+    
+    
     @Override
     public void run() {
      // Inicializaciones
-     
-     while(true) {
-         scan();
-         execute();
-     }   
+        while(true) {
+            scan();
+            execute();
+        }   
     }
+    
+    
+    @Override
     public void onScannedRobot(ScannedRobotEvent e) {
         analiza(e);
         esquivar(e);
         circularTarget(e);
         apuntaYDisparar(e);
     }
+    
+    
+    @Override
     public void onHitWall(HitWallEvent e) {
         
     }
+    
+    
+    @Override
     public void onHitByBullet(HitByBulletEvent e) {
         
     }
+    
+    
+    @Override
     public void onBulletHit(BulletHitEvent e) {
         
     }
+    
+    
     /**
      * Metodo que analiza la situacion en el campo de batalla respecto al enemigo. Ajustamos la orientacion,
      * velocidad y el movimiento del radar y del cañón
@@ -39,6 +54,8 @@ public class Mrpotato extends AdvancedRobot {
     private void analiza(ScannedRobotEvent e) {
         
     }
+    
+    
     /**
      * Método usado para identificar la posición del enemigo a través de una política de target circular
      * @param e 
@@ -46,6 +63,8 @@ public class Mrpotato extends AdvancedRobot {
     private void circularTarget(ScannedRobotEvent e) {
         
     }
+    
+    
     /**
      * Método que permite al tanque apuntar y disparar a la posición donde predecimos que estará el enemigo.
      * @param e Permite obtener información del enemigo
@@ -53,6 +72,8 @@ public class Mrpotato extends AdvancedRobot {
     private void apuntaYDisparar(ScannedRobotEvent e) {
         
     }
+    
+    
     /**
      * Método que permite al robot esquivar un disparo.
      * @param e 
