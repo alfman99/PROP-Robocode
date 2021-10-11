98f994379ec55c7ede1f717a858105b824a4d7f9
Okamsito <aleherpar@hotmail.com>
2021-10-11 18:16:02 +0200

    Metodos principales y estructura

--- Robocode/src/PROP/robot/Mrpotato.java
+++ Robocode/src/PROP/robot/Mrpotato.java
@@ -9,6 +9,56 @@ import robocode.*;
 public class Mrpotato extends AdvancedRobot {
     @Override
     public void run() {
+     // Inicializaciones
+     
+     while(true) {
+         scan();
+         execute();
+     }   
+    }
+    public void onScannedRobot(ScannedRobotEvent e) {
+        analiza(e);
+        esquivar(e);
+        circularTarget(e);
+        apuntaYDisparar(e);
+    }
+    public void onHitWall(HitWallEvent e) {
         
     }
+    public void onHitByBullet(HitByBulletEvent e) {
+        
+    }
+    public void onBulletHit(BulletHitEvent e) {
+        
+    }
+    /**
+     * Metodo que analiza la situacion en el campo de batalla respecto al enemigo. Ajustamos la orientacion,
+     * velocidad y el movimiento del radar y del cañón
+     * @param e Permite obtener información del enemigo
+     */
+    private void analiza(ScannedRobotEvent e) {
+        
+    }
+    /**
+     * Método usado para identificar la posición del enemigo a través de una política de target circular
+     * @param e 
+     */
+    private void circularTarget(ScannedRobotEvent e) {
+        
+    }
+    /**
+     * Método que permite al tanque apuntar y disparar a la posición donde predecimos que estará el enemigo.
+     * @param e Permite obtener información del enemigo
+     */
+    private void apuntaYDisparar(ScannedRobotEvent e) {
+        
+    }
+    /**
+     * Método que permite al robot esquivar un disparo.
+     * @param e 
+     */
+    private void esquivar(ScannedRobotEvent e) {
+        
+    }
+    
 }
